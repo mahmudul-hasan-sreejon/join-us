977f2d2274f93aa4213eb285fc9bcdbe9cd46ba4
mahmudul-hasan-sreejon <>
2018-12-24 15:02:49 +0600

    rendering the home page

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mysql = require("mysql");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 // connecting to the database
 const connection = mysql.createConnection({
     host: "localhost",
@@ -12,17 +14,19 @@ const connection = mysql.createConnection({
     database: "join_us_app"
 });
 
+// home page
 app.get("/", function(req, res) {
     const sqlString = "SELECT COUNT(*) AS total_users FROM users";
     connection.query(sqlString, function(err, results) {
         if(err) throw err;
         const total_users = results[0].total_users;
-        res.send("We have " + String(total_users) + " users.");
+        // res.send("We have " + String(total_users) + " users.");
+        res.render("home");
     });
 });
 
 app.get("/joke", function(req, res) {
-    res.send("Are you kidding me !?!");
+    res.render("home");
 });
 
 app.get("/num", function(req, res) {
@@ -30,6 +34,7 @@ app.get("/num", function(req, res) {
     res.send("The number is : " + num);
 });
 
+// Running node server
 app.listen(8080, function() {
     console.log("Server running on 8080 port...");
 });
