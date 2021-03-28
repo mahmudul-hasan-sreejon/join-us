04119a6ffc4837212dba878a8649368cca8d9df2
mahmudul-hasan-sreejon <mahmudul.hasan.sreejon@gmail.com>
2021-03-28 20:27:29 +0600

    Code cleanup

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const app = express();
 
-const app = express(); // created an express app
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
-app.set("view engine", "ejs"); // set default view engine to ejs
-app.use(bodyParser.urlencoded({extended: true})); // parse requests of content-type
-app.use(express.static(__dirname + "/public")); // make public directory accessible to views
-
-
-// required user routes
 require('./app/routes/user.routes.js')(app);
 
-
-// listen for requests
-const port = process.env.PORT || 8080; // Port mapping
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`Listening on port ${port}...`);
+	console.log(`Listening on port ${port}...\n`);
 });
