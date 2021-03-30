254a134f2135f4d4b01d28f3015afc14c3b1b4ee
mahmudul-hasan-sreejon <mahmudul.hasan.sreejon@gmail.com>
2021-03-30 21:50:49 +0600

    Code cleanup

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -9,10 +9,10 @@ const connection = mysql.createConnection(dbConfig);
 connection.connect((err) => {
 	// checking connection status
 	if (err) {
-		console.log('Could not connect to the database. Exiting now...', err);
+		console.log('Could not connect to the database. Exiting now...\n', err);
 		process.exit();
 	}
-	console.log('Successfully connected to the database');
+	console.log('Successfully connected to the database.\n');
 });
 
 module.exports = connection;
